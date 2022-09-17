75e1a77132b383eec0e2288820272270c0d3ffce
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-09-17 13:47:43 +0600

    dynamic ratings added

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,16 @@ const getProducts = async ()=>{
 
 // displayProducts items
 
-const displayProducts = (products) =>{
-    products.forEach((product) =>{
+const displayProducts = async (products) =>{
+   await products.forEach((product) =>{
         // destructuring product data
         const {id , title , price , category , image, rating :{rate}} = product
-        console.log(title.length);
+         
+        // set product rating to integer value 
+        const rating = (rate / 5) * 100
+        // calculating star width 
+        const percentage = `${Math.floor(rating / 10) * 10}%`
+
         // capturing product grid area from html
         const productGrid = document.getElementById('product-grid')
 
@@ -33,14 +38,12 @@ const displayProducts = (products) =>{
           <h3 class="font-medium text-xl">${price}</h3>
           <div class="card-actions justify-between items-center">
              <div class="outer">
-                <div class="inner">
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
-                    <i class="fa-solid fa-star"></i>
+                <div class="inner" style="width:${percentage}">
+                   
                 </div>
+              
              </div>
+             <span>${rate}</span>
             <button onclick="addToCart(${id})" class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-5 font-semibold">Add to cart <i class="fa-solid fa-cart-shopping text-white text-lg"></i></button>
           </div>
         </div> 

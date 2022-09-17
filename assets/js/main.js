91ffd00aff4f8ea7a8a59a3eb7ed06f1fc2913e1
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-09-17 17:49:08 +0600

    product-added-localstorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,8 @@
+// variables
+
+const quantity = document.getElementById('quantity')
+const productGrid = document.getElementById('product-grid')
+
 // fetching product data from fakeProduct api
 
 const getProducts = async ()=>{
@@ -6,6 +11,12 @@ const getProducts = async ()=>{
     const data = await res.json();
     displayProducts(data)
  }
+
+//  baskets
+
+let baskets = JSON.parse(localStorage.getItem('cart')) || [] ;
+
+console.log(baskets);
 
 // displayProducts items
 
@@ -18,10 +29,6 @@ const displayProducts = async (products) =>{
         const rating = (rate / 5) * 100
         // calculating star width 
         const percentage = `${Math.floor(rating / 10) * 10}%`
-
-        // capturing product grid area from html
-        const productGrid = document.getElementById('product-grid')
-
         // creating product card
         const productCard = document.createElement('div')
     
@@ -34,17 +41,15 @@ const displayProducts = async (products) =>{
         <figure><img class="w-full h-80 object-fill" src="${image}" alt="${title}" /></figure>
         <div class="card-body">
           <div class="badge bg-blue-600 border-0 text-white">${category}</div> 
-          <h2 class="card-title tooltip text-left text-lg" data-tip="${title}">${title.length > 20 ? title.slice(0 , 20) + '...' : title}</h2>
-          <h3 class="font-medium text-xl">${price}</h3>
+          <h2 class="card-title tooltip text-left text-lg" data-tip="${title}">${title.length > 26 ? title.slice(0 , 26) + '...' : title}</h2>
+          <h3 class="font-medium text-xl">$${parseInt(price)}</h3>
           <div class="card-actions justify-between items-center">
              <div class="outer">
-                <div class="inner" style="width:${percentage}">
-                   
-                </div>
-              
+                <div class="inner" style="width:${percentage}"></div>
              </div>
              <span>${rate}</span>
-            <button onclick="addToCart(${id})" class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-5 font-semibold">Add to cart <i class="fa-solid fa-cart-shopping text-white text-lg"></i></button>
+           
+             <button onclick="addToCart('${id}', '${title.includes("'") ? title.replace("'" , ""): title}' , ' ${price}', '${image}')" class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-5 font-semibold">Add to cart <i class="fa-solid fa-cart-shopping text-white text-lg"></i></button>
           </div>
         </div> 
         `
@@ -53,5 +58,43 @@ const displayProducts = async (products) =>{
     })
 }
 
-// calling get products function
-getProducts();
+// addToCart function
+const addToCart = (id , title , price , image) => {
+    // checking if selected item is already in cart or not
+    let searchProduct = baskets.find((product)=> product.id === id); 
+    if(searchProduct === undefined){
+         // pushing item to cart
+         baskets.push({
+            id :id,
+            title :title,
+            price :price,
+            image :image,
+            item : 1,
+        })
+
+    }
+     else{
+       return ;
+    }
+
+     // saving data to localStorage
+     localStorage.setItem('cart', JSON.stringify(baskets))
+
+     // updating cart quantity
+     updateQuantity()
+    
+}
+
+// update quantity value
+
+const updateQuantity = () => {
+    console.log(baskets.length);
+    quantity.innerText = baskets.map((product) => product.item).reduce((x,y) => x + y , 0);
+}
+
+
+
+// calling necessary function
+
+getProducts();    // display product function
+updateQuantity()  // update quantity value

e783ba9dc91185f5db1895c080c244b3e4c0a176
Mukut Chakrabarty <mukutckb1@gmail.com>
2022-09-17 13:05:44 +0600

    products-added

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+// fetching product data from fakeProduct api
+
+const getProducts = async ()=>{
+    const url = 'https://fakestoreapi.com/products'
+    const res = await fetch(url)
+    const data = await res.json();
+    displayProducts(data)
+ }
+
+// displayProducts items
+
+const displayProducts = (products) =>{
+    products.forEach((product) =>{
+        // destructuring product data
+        const {id , title , price , category , image, rating :{rate}} = product
+        console.log(title.length);
+        // capturing product grid area from html
+        const productGrid = document.getElementById('product-grid')
+
+        // creating product card
+        const productCard = document.createElement('div')
+    
+        // adding productCard className
+        productCard.classList.add('card' ,'bg-base-100' , 'shadow-md' ,'rounded-md')
+
+        // pushing product data to productCard
+
+        productCard.innerHTML = `
+        <figure><img class="w-full h-80 object-fill" src="${image}" alt="${title}" /></figure>
+        <div class="card-body">
+          <div class="badge bg-blue-600 border-0 text-white">${category}</div> 
+          <h2 class="card-title tooltip text-left text-lg" data-tip="${title}">${title.length > 20 ? title.slice(0 , 20) + '...' : title}</h2>
+          <h3 class="font-medium text-xl">${price}</h3>
+          <div class="card-actions justify-between items-center">
+             <div class="outer">
+                <div class="inner">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                </div>
+             </div>
+            <button onclick="addToCart(${id})" class="bg-blue-600 hover:bg-blue-500 text-white py-2 px-5 font-semibold">Add to cart <i class="fa-solid fa-cart-shopping text-white text-lg"></i></button>
+          </div>
+        </div> 
+        `
+        // appending to productGrid
+        productGrid.appendChild(productCard);
+    })
+}
+
+// calling get products function
+getProducts();
